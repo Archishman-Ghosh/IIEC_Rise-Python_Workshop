53d2f5c12735866df08926a5db3c23fb484e4f84
Archishman Ghosh <67512855+Archishman-Ghosh@users.noreply.github.com>
2020-08-21 17:26:30 +0530

    Query Based Program to launch Applications from the OS
    This is a solution to the challenge given by Mr Vimal Daga sir as part of the IIEC Rise Python Program.
    This code is as per a Windows machine and only has generic applications that we commonly use. It can be made more versatile and flexible by including more applications but that would be dependent on the user.

---
+++ menu-based-os.py
@@ -0,0 +1,18 @@
+import os
+while True:
+#    print("Which application do you want to launch?")
+    inp = input("Which application do you want to launch?")
+    if 'launch' in inp or 'run' in inp or 'start' in inp or 'execute' in inp:
+        if 'notepad' in inp or 'editor' in inp:
+            os.system('start notepad')
+        elif 'chrome' in inp or 'browser' in inp or 'internet' in inp:
+            os.system('start chrome')
+        elif 'player' in inp or 'video' in inp:
+            os.system('start KMPlayer64')
+        else:
+            print("Doesn't support")
+    elif 'exit' in inp:
+        break
+    else:
+        print("Doesn't support")
+        
